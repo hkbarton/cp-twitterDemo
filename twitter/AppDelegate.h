376596d754e8b8547256f1edd3b5de02308ffd21
kehuang <kehuang@box.com>
2015-02-16 15:14:24 -0800

    Initial Commit

---
+++ twitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  twitter
+//
+//  Created by Ke Huang on 2/16/15.
+//  Copyright (c) 2015 Ke Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
